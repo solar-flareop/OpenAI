8cda8202d88050c2579e599afdf156e4625b0b9e
Suraj Pattade <surajpattade@gmail.com>
2023-04-03 02:10:12 +0530

    change in server file

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.use(errorHandler);
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://toolkit.onrender.com", "http://localhost:5173"],
+    origin: ["https://protoolkit.netlify.app"],
   })
 );
 
